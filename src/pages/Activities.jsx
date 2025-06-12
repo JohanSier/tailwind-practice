46aa7d24c06ebf37e8b711090a9bdbc60430e729
Johan Sierra <johansierra222@gmail.com>
2025-06-12 15:44:15 -0500

    feat: add activities component, write code on it later

---
+++ src/pages/Activities.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Overlay from '../components/Overlay'
+
+const Activities = () => {
+  return (
+    <>
+      <Overlay/>
+    </>
+  )
+}
+
+export default Activities
