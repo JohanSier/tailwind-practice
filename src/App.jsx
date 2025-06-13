53c1d8ca8b35ce578183496f23ebd2168c9175f2
Johan Sierra <johansierra222@gmail.com>
2025-06-13 16:43:33 -0500

    feat: add sistemas oscilantes page (first book)

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import "./index.css"
 import Activities from "./pages/Activities.jsx";
+import SistemasOscilantes from "./pages/SistemasOscilantes.jsx";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index path="/" element={<Home />} />
-          <Route path='actividades' element={<Activities/>} />
+          <Route path='actividades' element={<Activities/>}>
+            <Route path='sistemas-oscilantes' element={<SistemasOscilantes/>} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/pages/SistemasOscilantes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SistemasOscilantes = () => {
+  return (
+    <div>
+      <h1>Sistemas Oscilantes</h1>
+    </div>
+  )
+}
+
+export default SistemasOscilantes
