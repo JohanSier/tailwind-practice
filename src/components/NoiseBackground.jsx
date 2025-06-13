2bdae6c4ebc388ea1503ecf76ecdc35889af855d
Johan Sierra <johansierra222@gmail.com>
2025-06-13 11:13:44 -0500

    feat: create noise background component to add it to all pages
    - add noise background component to Overlay, Home and Activities pages
    - add animation keyframes of noise background to index.css to be able to access it in any page

---
+++ src/components/NoiseBackground.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ruidoVisual from "../assets/images/three-d/granular.png";
+import "../index.css";
+
+const NoiseBackground = () => {
+  return (
+    <div
+    className="fixed top-0 left-0 w-[300%] h-[300%] opacity-5 z-[-10] pointer-events-none animate-noise"
+    style={{
+      backgroundImage: `url(${ruidoVisual})`,
+      backgroundRepeat: "repeat",
+      imageRendering: "pixelated",
+    }}
+  />
+  );
+};
+
+export default NoiseBackground;
