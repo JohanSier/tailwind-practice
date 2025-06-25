144aad751828b75e138ae5c2009b565c6db99bad
Johan Sierra <johansierra222@gmail.com>
2025-06-25 17:59:55 -0500

    fix: fix draggable cards bounds
    - Make the draggable cards only be draggable within its parent container
    - Add draggable prop to DraggableCard component

--- src/components/DraggableCard.jsx
+++ src/components/DraggableCard.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import Draggable from "react-draggable";
+import { useRef } from "react";
+import React from "react";
 
-const DraggableCard = ({title, content}) => {
-  return (
-    <div style={{fontFamily: "Montserrat, sans-serif"}} className='w-[150px] h-fit flex items-center justify-center py-10 px-15 rounded-xl'>
-      <div className="flex items-center w-[70%] bg-emerald-500 rounded-full">
-        <p className="text-lg text-center">{title}</p>
+const DraggableCard = ({ title, content, draggable = false, className = ""}) => {
+  const nodeRef = useRef(null);
+
+  const card = (
+    <div
+      ref={nodeRef}
+      style={{ fontFamily: "Montserrat, sans-serif" }}
+      className={`cursor-pointer bg-white border border-[#868282] w-[250px] h-fit flex flex-col items-center justify-center pt-3 pb-5 px-5 rounded-xl ${className}`}
+    >
+      <div className="flex border border-[#1e1e1e] py-1 mb-3 justify-center items-center w-[200px] bg-[#1e1e1e] rounded-full">
+        <p className="text-lg text-white text-center">{title}</p>
       </div>
-      <p className="text-lg text-center">{content}</p>
+      <p className="w-full text-[0.8rem] text-center">{content}</p>
     </div>
-  )
-}
+  );
 
-export default DraggableCard
+  return draggable ? 
+  (<Draggable bounds="parent" nodeRef={nodeRef}>{card}</Draggable>) : (card);
+};
+
+export default DraggableCard;
